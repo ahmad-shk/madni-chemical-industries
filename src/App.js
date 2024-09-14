415fb8cefdbbac5e1217b175d7278278c06c9bf5
Ahmad Saleem <ahmadsaleem19950@gmail.com>
2024-09-14 16:35:55 +0500

    updtae

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import './App.css';
 import Layout from './theme/layout'
 function App() {
 
-  const [count,setCount]=useState(1)
-  const bgColors = ['bg-white', 'bg-[#c9c9c9]', 'bg-[#252525]', 'bg-[#0d67a5]'];
-  const fgColor = ['bg-[#0d67a5]', 'bg-[#0d67a5]', 'bg-[#343434]' ,'bg-[#c9c9c9]']
+  // const [count,setCount]=useState(1)
+  // const bgColors = ['bg-white', 'bg-[#c9c9c9]', 'bg-[#252525]', 'bg-[#0d67a5]'];
+  // const fgColor = ['bg-[#0d67a5]', 'bg-[#0d67a5]', 'bg-[#343434]' ,'bg-[#c9c9c9]']
 
-  const handleClick = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
+  // const handleClick = () => {
+  //   setCount((prevCount) => prevCount + 1);
+  // };
 
   return (
     <>
-    <div className='px-12 py-6 bg-[#383838] '>
+    {/* <div className='px-12 py-6 bg-[#383838] '>
       <h1 className='text-white text-center text-[28px] font-bold'>CLICK TO CHANGE THEME</h1>
       <div className='flex flex-wrap gap-8 justify-center pt-6'>
    
     <button onClick={()=>handleClick()} className='endure-btn border bg-[#c9c9c9] px-4 py-2 rounded-full'>Click</button>
    
     </div>
-    </div>
+    </div> */}
  
-   <Layout backgroundColor={bgColors[count % 4]} foregroundColor={fgColor[count % 4]}/> 
+   <Layout /> 
 
     </>
   );
